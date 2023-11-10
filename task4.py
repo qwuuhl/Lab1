6824aecbb9edb563035ed3cbf094aa9d3d4764ee
qwuuhl <149656338+qwuuhl@users.noreply.github.com>
2023-11-10 08:47:49 +0200

    Update task4.py

--- task4.py
+++ task4.py
@@ -6,15 +6,10 @@
 тобто таким, який однаково читається як справа наліво,
 так і зліва направо. Для літер регістр не враховувати.
 Приклади рядків-паліндромів: racecar, 10201, Ada, Never odd or even
+Автор: Задворна Анастасія Богданівна
 """
 
-row = input('рядок: ').lower()
-origin_list = [i for i in [*row] if i != ' ']
-new_list = list(reversed([i for i in [*row] if i != ' ']))
-counter = 0
-
-for index, char in enumerate(origin_list):
-    if char == new_list[index]:
-        counter += 1
-
-print('паліндром') if counter == len(origin_list) else print('не паліндром')
+text = input ("Введіть рядок: ")
+text = text.replace(" ", '')
+text = text.lower()
+print(text == text[::-1])
